56275917e63e8e0177ea14d3b91a2d0671d6b6ad
Jache02 <143908363+Jache02@users.noreply.github.com>
2024-01-13 20:13:29 -0800

    Se agregaron archivos javascript

---
+++ Boletines/javascript/index.js
@@ -0,0 +1,29 @@
+//Para index.html
+let moreinfo = document.querySelectorAll(".vermas");
+
+//Recorrerlos
+
+moreinfo.forEach(function(more){
+
+    //Agregar un evento click a cada uno de ellos
+    more.addEventListener("click",function(ev){
+        ev.preventDefault();
+        let content = document.querySelector('.content');
+
+        //Quitar las clases de animacio que ya tiene
+        content.classList.remove("animate__fadeInDown");
+        content.classList.remove("animate__animated");
+
+        //Agregar clases para animar su salida fadeOutUp
+        content.classList.add("animate__fadeOutUp");
+        content.classList.add("animate__animated");
+
+        setTimeout(function(){
+            location.href= "/Restaurantes/Horno-Di-Parma.html";
+        },850);
+        //setInterval
+
+        return false;
+    });
+});
+
---
+++ Boletines/javascript/main.js
@@ -0,0 +1,58 @@
+
+
+//DOM
+/*
+let links = document.querySelectorAll("a");
+
+links.forEach(function(link){
+    console.log(links);
+});
+*/
+/*
+let links = document.querySelectorAll(".close");
+
+links.forEach(function(link){
+    link.addEventListener('click',function(ev){
+        ev.preventDefault();
+
+        return false;
+    });
+    
+}); 
+*/
+/*
+let iconos = document.querySelectorAll("i");
+
+iconos.forEach(function(icono){
+    icono.classList.remove("fa-regular"); 
+    icono.classList.add("fa-solid"); 
+})
+*/
+//Obtener los elementos de la clase .close
+let links = document.querySelectorAll(".close");
+
+//Recorrerlos
+
+links.forEach(function(link){
+
+    //Agregar un evento click a cada uno de ellos
+    link.addEventListener("click",function(ev){
+        ev.preventDefault();
+        let content = document.querySelector('.content');
+
+        //Quitar las clases de animacio que ya tiene
+        content.classList.remove("animate__fadeInDown");
+        content.classList.remove("animate__animated");
+
+        //Agregar clases para animar su salida fadeOutUp
+        content.classList.add("animate__fadeOutUp");
+        content.classList.add("animate__animated");
+
+        setTimeout(function(){
+            location.href= "../index.html";
+        },850);
+        //setInterval
+
+        return false;
+    });
+});
